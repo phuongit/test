e0004a6f9d68153c84986bc494623ea0045e8d60
phuongit <phuongk3a@gmail.com>
2019-09-26 10:55:15 +0700

    test1

---
+++ ip.component.ts
@@ -0,0 +1,20 @@
+import {Component} from 'angular/core';
+import { IpService } from './ip.service';
+
+@Component(
+    {
+        selector: 'app-ip',
+        template: '<h3>{{ip}}</h3>',
+        providers: [IpService]
+    }
+)
+export class IpComponent{
+    ip: String;
+    constructor(private ipService: IpService){
+        this.ipService.getIp()
+        .then(ip => this.ip =  ip)
+        .catch(err =>console.log(err));
+
+    }
+   
+}
